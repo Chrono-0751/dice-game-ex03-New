dcbc5d243eb032264b58c3d8e5a3682d89abd9a3
chrono7 <hirotomo17830@gmail.com>
2025-06-27 12:02:55 +0900

    Dice game; initial revision

---
+++ dicegame.java
@@ -0,0 +1,18 @@
+package ex03.dice;
+import java.util.Random;
+
+public class dicegame {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int dice1 = random.nextInt(6)+1;
+        int dice2 = random.nextInt(6)+1;
+        int sum = (int)(dice1 + dice2);
+
+        System.out.println("Rolling dice...");
+        System.out.println("Die 1: " + dice1);
+        System.out.println("Die 2: " + dice2);
+        System.out.println("Total value: " + sum);
+    }
+}
