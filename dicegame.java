fdb92d13f156a6ce1f31d59fd7b765a32a051022
chrono7 <hirotomo17830@gmail.com>
2025-06-27 12:13:51 +0900

    Greetings added

--- dicegame.java
+++ dicegame.java
@@ -1,14 +1,21 @@
 package ex03.dice;
 import java.util.Random;
+import java.util.Scanner;
 
 public class dicegame {
     public static void main(String[] args) {
 
         Random random = new Random();
+        Scanner sc = new Scanner(System.in);
 
         int dice1 = random.nextInt(6)+1;
         int dice2 = random.nextInt(6)+1;
         int sum = (int)(dice1 + dice2);
+
+
+        System.out.printf("What is your name?\n> ");
+        String name = sc.next();
+        System.out.println("Hello, " + name + "!");
 
         System.out.println("Rolling dice...");
         System.out.println("Die 1: " + dice1);
